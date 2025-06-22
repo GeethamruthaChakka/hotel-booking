a2ddfac78bfdfd796204ed83b991b320a44d0fc7
GeethamruthaChakka <geethamruthach@gmail.com>
2025-06-22 20:45:12 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components-temp/Hero'
+import FeaturedDestination from '../components-temp/FeaturedDestination'
+import ExclusiveOffers from '../components-temp/ExclusiveOffers'
+import Testimonial from '../components-temp/Testimonial'
+import NewsLetter from '../components-temp/NewsLetter'
+const Home=()=>{
+    return(
+        <>
+            <Hero/>
+            <FeaturedDestination/>
+            <ExclusiveOffers />
+            <Testimonial />
+            <NewsLetter />
+        </>
+    )
+}
+export default Home
